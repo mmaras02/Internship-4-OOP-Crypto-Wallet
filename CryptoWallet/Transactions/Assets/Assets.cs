a87492bf46480cc3735683aa56c8db83c44e6255
mmaras02 <mmaras02@fesb.hr>
2022-12-07 15:56:50 +0100

    function transaction compleated

---
+++ CryptoWallet/Transactions/Assets/Assets.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using CryptoWallet.Assets;
+
+
+namespace CryptoWallet.Assets
+{
+    public abstract class Assets
+    {
+        public Guid Address{get;}
+        public string Name{get;}
+        //public double Value{get;set;}
+
+        public string AssetType{get;set;}
+        //private double Value{get;set;}
+
+        public Assets(string name,double value)
+        {
+            Address=Guid.NewGuid();
+            Name=name;
+            //Value=value;
+            AssetType="";
+        }
+
+    }
+}
---
+++ CryptoWallet/Transactions/Assets/NonFungibleAssets.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using CryptoWallet.Assets;
+
+//non-fungible tokens are non-interchangeable, unique, indivisible, and irreplaceable
+//They can’t be exchanged for one another without losing value because each token is unique.
+namespace CryptoWallet.Assets
+{
+    public  class NonFungibleAssets:Assets
+    {
+        //public double Value { get; set; }
+        public Guid AllowedFungibleAssetAddress{get;set;}//adresa fungible asseta
+        //dodat currency?
+        public NonFungibleAssets(string name,double value,Guid supportedFungibleAssetAddress):base(name,value)
+        {
+            //Value=value;
+            AllowedFungibleAssetAddress=supportedFungibleAssetAddress;
+            AssetType="Non fungible asset";
+        }
+        // public Guid GetFungibleAssetAddress()
+        // {
+        //     return AllowedFungibleAssetAddress;
+        // }
+   /*      public bool FindSupportedFungibleAsset(List<FungibleAssets>fungibleAssetList,Guid nonFungible)
+        {
+            fungibleAssetList.Find(x=>x.Address.Equals())
+            foreach(var item in fungibleAssetList)
+            {
+
+            }
+            for(var item=0;item<AllowedAssets.Count;item++)
+                if(AllowedFungibleAssetAddress.Equals(AllowedAssets[item]))
+                    return true;
+            return false;
+        } */
+
+        
+    }
+}
